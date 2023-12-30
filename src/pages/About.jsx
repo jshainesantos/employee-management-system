9bbe6c6183ebdb0e4416c0e04baed3d7b6a830d7
jshainesantos <jessicashainesantos@gmail.com>
2023-12-30 15:20:07 +0800

    fixed about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,25 +16,25 @@ function About() {
       </div>
       <div className="row justify-content-center">
         <div className="col-12 col-md-6">
-          <h2 className="h5 mb-3">Key Features:</h2>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <i className="bi bi-check-circle text-success me-2"></i> Create:
+          <h2 className="h5 mb-4">Key Features:</h2>
+          <ul className="list-group">
+            <li className="list-group-item pt-3">
+              <i className="bi bi-check-circle text-success me-2 fw-bold">✓</i> Create:
               Users can add new employees by filling in first name, last name,
               and grade fields.
             </li>
-            <li className="list-group-item">
-              <i className="bi bi-lock text-primary me-2"></i> Read: The
+            <li className="list-group-item pt-3">
+              <i className="bi bi-lock text-primary me-2 fw-bold">✓</i> Read: The
               application fetches and displays a list of existing employee
               records from Firebase.
             </li>
-            <li className="list-group-item">
-              <i className="bi bi-lock text-primary me-2"></i> Update: Users can
+            <li className="list-group-item pt-3">
+              <i className="bi bi-lock text-warning me-2 fw-bold">✓</i> Update: Users can
               edit existing employee records by clicking an <b>Edit</b> button.
             </li>
 
-            <li className="list-group-item">
-              <i className="bi bi-search text-warning me-2"></i> Delete: Users
+            <li className="list-group-item pt-3">
+              <i className="bi bi-search text-danger me-2 fw-bold">✓</i> Delete: Users
               can delete employee records using a <b>Delete</b> button.
             </li>
           </ul>
